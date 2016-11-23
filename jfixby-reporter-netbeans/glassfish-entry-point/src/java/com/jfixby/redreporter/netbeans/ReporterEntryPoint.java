c7dd32a1c56fb4c6473c7d87385d8b537e6598bf
J Fixby <github@jfixby.com>
2016-11-23 13:57:29 +0100

    +nb

---
+++ jfixby-reporter-netbeans/glassfish-entry-point/src/java/com/jfixby/redreporter/netbeans/ReporterEntryPoint.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jfixby.redreporter.netbeans;
+
+import com.jfixby.redreporter.glassfish.TestProcessor;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author JCode
+ */
+public class ReporterEntryPoint extends TestProcessor {}
