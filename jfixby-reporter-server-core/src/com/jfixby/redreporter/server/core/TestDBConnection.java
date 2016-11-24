717acb0cc443113171656ecc5eb704baf3552a24
J Fixby <github@jfixby.com>
2016-11-24 15:58:53 +0100

    ...

--- jfixby-reporter-server-core/src/com/jfixby/redreporter/server/core/TestDBConnection.java
+++ jfixby-reporter-server-core/src/com/jfixby/redreporter/server/core/TestDBConnection.java
@@ -1,8 +1,30 @@
+
 package com.jfixby.redreporter.server.core;
+
+import com.jfixby.cmns.adopted.gdx.json.RedJson;
+import com.jfixby.cmns.api.file.LocalFileSystem;
+import com.jfixby.cmns.api.json.Json;
+import com.jfixby.cmns.db.api.DB;
+import com.jfixby.cmns.db.api.TestConnectionResult;
+import com.jfixby.cmns.db.mysql.MySQL;
+import com.jfixby.cmns.db.mysql.MySQLConfig;
+import com.jfixby.red.desktop.DesktopSetup;
 
 public class TestDBConnection {
 
-	public static void main (String[] args) {
+	public static void main (final String[] args) {
+
+		DesktopSetup.deploy();
+		Json.installComponent(new RedJson());
+
+		LocalFileSystem.ApplicationHome().child("credentials");
+
+		final MySQLConfig config = new MySQLConfig();
+
+		DB.installComponent(new MySQL(config));
+
+		final TestConnectionResult test = DB.testConnection();
+		test.print();
 
 	}
 
