1f3cf145f869ac5fbb4c03c4643a5bdd4e538614
J Fixby <github@jfixby.com>
2017-01-03 15:44:03 +0100

    report start

--- jfixby-reporter-api/src/com/jfixby/redreporter/api/analytics/AnalyticsReporterEvents.java
+++ jfixby-reporter-api/src/com/jfixby/redreporter/api/analytics/AnalyticsReporterEvents.java
@@ -4,5 +4,6 @@ package com.jfixby.redreporter.api.analytics;
 public class AnalyticsReporterEvents {
 
 	public static final String SERVICE_START = "AnalyticsReporter.SERVICE_START";
+	public static final String APPLICATION_EXIT = "AnalyticsReporter.APPLICATION_EXIT";
 
 }
--- jfixby-reporter-red/src/com/jfixby/redreporter/analytics/RedAnalyticsReporter.java
+++ jfixby-reporter-red/src/com/jfixby/redreporter/analytics/RedAnalyticsReporter.java
@@ -6,15 +6,18 @@ import com.jfixby.redreporter.api.analytics.AnalyticsReporterEvents;
 import com.jfixby.redreporter.api.analytics.ReportWriter;
 import com.jfixby.redreporter.api.transport.ReporterTransport;
 import com.jfixby.scarabei.api.debug.Debug;
+import com.jfixby.scarabei.api.sys.OnExitListener;
+import com.jfixby.scarabei.api.sys.Sys;
 import com.jfixby.scarabei.api.sys.settings.SystemSettings;
 import com.jfixby.scarabei.api.ver.Version;
 
-public class RedAnalyticsReporter implements AnalyticsReporterComponent {
+public class RedAnalyticsReporter implements AnalyticsReporterComponent, OnExitListener {
 	private final String authorID = ("com.red-triplane.reporter.analytics");
 	private final ReporterTransport transport;
 
 	public RedAnalyticsReporter (final ReporterTransport transport) {
 		this.transport = Debug.checkNull("transport", transport);
+		Sys.addOnExitListener(this);
 	}
 
 	public ReporterTransport getTransport () {
@@ -28,18 +31,27 @@ public class RedAnalyticsReporter implements AnalyticsReporterComponent {
 
 	@Override
 	public void reportStart () {
-		final ReportWriter writer = this.transport.newReportWriter();
-		writer.setAuthor(this.authorID);
-		writer.setSubject(AnalyticsReporterEvents.SERVICE_START);
-		writer.addStringValue(Version.Tags.VersionName, SystemSettings.getStringParameter(Version.Tags.VersionName));
-		writer.addStringValue(Version.Tags.PackageName, SystemSettings.getStringParameter(Version.Tags.PackageName));
-		writer.addStringValue(Version.Tags.VersionCode, SystemSettings.getStringParameter(Version.Tags.VersionCode));
-		writer.submitReport();
+		{
+			final ReportWriter writer = this.transport.newReportWriter();
+			writer.setAuthor(this.authorID);
+			writer.setSubject(AnalyticsReporterEvents.SERVICE_START);
+			writer.addStringValue(Version.Tags.VersionName, SystemSettings.getStringParameter(Version.Tags.VersionName));
+			writer.addStringValue(Version.Tags.PackageName, SystemSettings.getStringParameter(Version.Tags.PackageName));
+			writer.addStringValue(Version.Tags.VersionCode, SystemSettings.getStringParameter(Version.Tags.VersionCode));
+			writer.submitReport();
+		}
 	}
 
 	@Override
 	public ReportWriter newReportWriter () {
 		return this.transport.newReportWriter();
+	}
+
+	@Override
+	public void onExit () {
+		final ReportWriter writer = this.transport.newReportWriter();
+		writer.setAuthor(this.authorID);
+		writer.setSubject(AnalyticsReporterEvents.APPLICATION_EXIT);
 	}
 
 }
